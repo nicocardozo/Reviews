353743a9d346e1796601d446ee1851c6d2a4c033
Nicolò Cardozo <nic.cardozo@gmail.com>
2023-04-21 11:37:35 +0200

    Project Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,75 @@
+import people from './data'
+import { useState } from 'react'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
-};
-export default App;
+  const [index, setIndex] = useState(0)
+  const { name, job, image, text } = people[index]
+
+  const checkNumber = number => {
+    if (number < 0) {
+      return people.length - 1
+    }
+
+    if (number > people.length - 1) {
+      return 0
+    }
+
+    return number
+  }
+
+  const prevPerson = () => {
+    setIndex(currentIndex => {
+      const newIndex = (currentIndex - 1 + people.length) % people.length
+
+      return newIndex
+    })
+  }
+
+  const nextPerson = () => {
+    setIndex(currentIndex => {
+      const newIndex = (currentIndex + 1) % people.length
+      console.log(newIndex)
+
+      return newIndex
+    })
+  }
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length)
+
+    if (randomNumber === index) {
+      randomNumber += 1
+    }
+
+    setIndex(checkNumber(randomNumber))
+  }
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="btn btn-hipster" onClick={randomPerson}>
+          Random
+        </button>
+      </article>
+    </main>
+  )
+}
+export default App
